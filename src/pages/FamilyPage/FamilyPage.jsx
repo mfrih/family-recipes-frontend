2a0ab3f33e637e2ad150c8495f2fb8b54d0fa636
Maria Frih <maria.frih@gmail.com>
2024-02-12 22:36:23 +0100

    adds the display of the family name in the family page

--- src/pages/FamilyPage/FamilyPage.jsx
+++ src/pages/FamilyPage/FamilyPage.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AddMembers from "../../components/AddMembers/AddMembers";
 import MembersList from "../../components/MembersList/MembersList";
 import { useParams } from "react-router-dom";
+import myApi from "../../api/apiHandler";
 
-function FamilyPage() {
+const FamilyPage = () => {
+  const [family, setFamily] = useState(null);
   const { familyId } = useParams();
+
+  const fetchFamily = async () => {
+    try {
+      const response = await myApi.get(`api/families/${familyId}`);
+      setFamily(response.data);
+    } catch (error) {
+      console.error("Failed to fetch family:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFamily();
+  }, []);
+
+  if (!family) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
-      <h2>This is your family page</h2>
+      <h2>Welcome to "{family.name}"</h2>
       <AddMembers familyId={familyId} />
       <MembersList familyId={familyId} />
     </div>
   );
-}
+};
 
 export default FamilyPage;
