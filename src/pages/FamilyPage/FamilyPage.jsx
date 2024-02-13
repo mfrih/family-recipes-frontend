3cd9b687053631e9cb7209268b13467c0e057709
Maria Frih <maria.frih@gmail.com>
2024-02-13 18:36:45 +0100

    adds fetching family admins in FamilyPage to add some conditions on features

--- src/pages/FamilyPage/FamilyPage.jsx
+++ src/pages/FamilyPage/FamilyPage.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../../context/auth.context";
 import AddMembers from "../../components/AddMembers/AddMembers";
 import MembersList from "../../components/MembersList/MembersList";
 import { useParams } from "react-router-dom";
@@ -6,12 +7,19 @@ import myApi from "../../api/apiHandler";
 
 const FamilyPage = () => {
   const [family, setFamily] = useState(null);
+  const [isAdmin, setIsAdmin] = useState(false);
   const { familyId } = useParams();
+  const { user } = useContext(AuthContext);
 
   const fetchFamily = async () => {
     try {
       const response = await myApi.get(`api/families/${familyId}`);
-      setFamily(response.data);
+      const fetchedFamily = response.data;
+      setFamily(fetchedFamily);
+      const isAdminStatus = fetchedFamily.admins.some(
+        (adminId) => adminId === user._id
+      );
+      setIsAdmin(isAdminStatus);
     } catch (error) {
       console.error("Failed to fetch family:", error);
     }
@@ -28,8 +36,8 @@ const FamilyPage = () => {
   return (
     <div>
       <h2>Welcome to "{family.name}"</h2>
-      <AddMembers familyId={familyId} />
-      <MembersList familyId={familyId} />
+      <AddMembers familyId={familyId} isAdmin={isAdmin} />
+      <MembersList familyId={familyId} isAdmin={isAdmin} />
     </div>
   );
 };
