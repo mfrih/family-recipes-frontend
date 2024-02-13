83001f31ecee4fd6ab058ac4280698e577e54b1f
Maria Frih <maria.frih@gmail.com>
2024-02-13 15:48:40 +0100

    adds the families the recipes has been shared with in the OneRecipePage. Le petit Promise.all au passage ;-)

--- src/pages/OneRecipePage/OneRecipePage.jsx
+++ src/pages/OneRecipePage/OneRecipePage.jsx
@@ -4,20 +4,36 @@ import myApi from "../../api/apiHandler";
 
 function OneRecipePage() {
   const [recipe, setRecipe] = useState(null);
+  const [familyNames, setFamilyNames] = useState([]);
   const { recipeId } = useParams();
 
   const fetchRecipe = async () => {
     try {
       const response = await myApi.get(`api/recipes/${recipeId}`);
       setRecipe(response.data);
+      // get the familyId array from response.data to pass it to the fonction that will fetch family names
+      fetchFamilyNames(response.data.familyId);
     } catch (error) {
       console.error("Failed to fetch recipe:", error);
     }
   };
 
+  //fetch Family Names, since the recipe only has familyIds
+  const fetchFamilyNames = async (familyIds) => {
+    try {
+      const promises = familyIds.map((familyId) =>
+        myApi.get(`/api/families/${familyId}`)
+      );
+      const families = await Promise.all(promises);
+      setFamilyNames(families.map((family) => family.data.name));
+    } catch (error) {
+      console.error("Failed to fetch families for family names:", error);
+    }
+  };
+
   useEffect(() => {
     fetchRecipe();
-  }, []);
+  }, [recipeId]);
 
   if (!recipe) {
     return <p>Loading...</p>;
@@ -37,7 +53,15 @@ function OneRecipePage() {
       </div>
       <div>
         <h4>Families shared with</h4>
-        {/* todo : map ici les familles que l'on récupère dans la recette */}
+        {familyNames.length > 0 ? (
+          <ul>
+            {familyNames.map((name, index) => (
+              <li key={index}>{name}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>This recipe has not been shraed with any family, yet.</p>
+        )}
       </div>
     </div>
   );
